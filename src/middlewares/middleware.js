0097e7e768045ecd9719cdc337a3e9f81379fd49
GILVANSANTOS <gilvanf28@gmail.com>
2021-05-20 19:29:32 -0300

    projectJsAgendaFront

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,35 @@
+exports.middlewareGlobal = ( req, res , next ) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
+    next();
+};
+
+exports.outromiddleware = (req, res, next ) => {
+  next();
+};
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if(err){
+       return res.render('404'); 
+     } 
+    next();   
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
+//MONGODB   gilvansantos   218386gilvan
+
+//mongodb+srv://gilvansantos:<password>@cluster0.dseck.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
+exports.loginRequired = (req, res, next) =>  {
+  if(!req.session.user){
+    req.flash('errors', 'Você precisa fazer login');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+  next();
+};

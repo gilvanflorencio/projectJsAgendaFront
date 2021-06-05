1837d41560ae00736952b44c73d338450a27b0b2
GILVANSANTOS <gilvanf28@gmail.com>
2021-06-04 21:05:14 -0300

    deploy with googleCloud test

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,3 +4,8 @@ exports.index = async (req, res) => {
     const contatos = await Contato.buscaContatos();
     res.render('index', { contatos });
 };
+
+/*  reiniciar servidor
+ eval $(ssh-agent)    ssh-add ~/.ssh/id_rsa     ssh  e o IP da maquina
+*/
+
